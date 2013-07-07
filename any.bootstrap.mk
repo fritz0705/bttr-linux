a5d7fd0e673a793c5b4cd36672fef3788e26d1d0
Fritz Grimpen <fritz@grimpen.net>
2013-07-07 15:40:37 +0000

    Use git-ls-tree to get real package names

--- any.bootstrap.mk
+++ any.bootstrap.mk
@@ -1,1 +1,5 @@
+ifneq ($(shell git rev-parse HEAD),)
+PACKAGES := $(shell git ls-tree -r --name-only $$(git rev-parse HEAD) | grep "\/Makefile$$" | sed 's/\/Makefile$$//')
+else
 PACKAGES := $(shell find ./ -mindepth 1 -name Makefile -type f -printf "%h\n")
+endif
