1eb107c9271d03664be0cc1785b05f46d40001e8
Fritz Grimpen <fritz@grimpen.net>
2013-07-07 15:27:21 +0000

    More consistent use of symlinks for /lib64 and /lib

--- etc/filesystem/install-x86_64.mk
+++ etc/filesystem/install-x86_64.mk
@@ -1,3 +1,5 @@
+# On x86_64 platforms, we prefer a traditional, non-systemd filesystem layout
+
 install:
 	# /
 	install -dm755 $(DESTDIR)/boot $(DESTDIR)/dev $(DESTDIR)/etc $(DESTDIR)/home \
@@ -6,8 +8,8 @@ install:
 	install -dm555 $(DESTDIR)/proc $(DESTDIR)/sys
 	install -dm0750 $(DESTDIR)/root
 	install -dm1777 $(DESTDIR)/tmp
-	install -dm755 $(DESTDIR)/lib64
-	ln -sf lib64 $(DESTDIR)/lib
+	install -dm755 $(DESTDIR)/lib
+	ln -sf lib $(DESTDIR)/lib64
 	# /var
 	install -dm755 $(DESTDIR)/var/local $(DESTDIR)/var/opt $(DESTDIR)/var/log \
 		$(DESTDIR)/var/lib $(DESTDIR)/var/cache $(DESTDIR)/var/empty
@@ -15,11 +17,11 @@ install:
 	# /usr
 	install -dm755 $(DESTDIR)/usr/bin $(DESTDIR)/usr/include $(DESTDIR)/usr/sbin \
 		$(DESTDIR)/usr/share $(DESTDIR)/usr/src $(DESTDIR)/usr/lib
-	ln -sf lib $(DESTDIR)/usr/lib64
+	ln -sf lib $(DESTDIR)/usr/lib
 	# /usr/local
 	install -dm755 $(DESTDIR)/usr/local/bin $(DESTDIR)/usr/local/games \
 		$(DESTDIR)/usr/local/etc $(DESTDIR)/usr/local/include \
-		$(DESTDIR)/usr/local/lib $(DESTDIR)/usr/local/lib64 \
+		$(DESTDIR)/usr/local/lib $(DESTDIR)/usr/local/lib \
 		$(DESTDIR)/usr/local/man $(DESTDIR)/usr/local/sbin \
 		$(DESTDIR)/usr/local/share $(DESTDIR)/usr/local/src
 	ln -sf lib $(DESTDIR)/usr/local/lib64
