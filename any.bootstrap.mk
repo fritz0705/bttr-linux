9d4385204c2790eee92d2f4ed84b18278d87ca45
Fritz Grimpen <fritz@grimpen.net>
2013-10-05 19:07:35 +0000

    Support any builds on subdirs

--- any.bootstrap.mk
+++ any.bootstrap.mk
@@ -1,5 +1,9 @@
+ifndef SUBDIR
+SUBDIR := ./
+endif
+
 ifneq ($(shell git rev-parse HEAD),)
-PACKAGES := $(shell git ls-tree -r --name-only $$(git rev-parse HEAD) | grep "\/Makefile$$" | sed 's/\/Makefile$$//')
+PACKAGES := $(shell git ls-tree -r --name-only $$(git rev-parse HEAD) $(SUBDIR) | grep "\/Makefile$$" | sed 's/\/Makefile$$//')
 else
-PACKAGES := $(shell find ./ -mindepth 1 -name Makefile -type f -printf "%h\n")
+PACKAGES := $(shell find $(SUBDIR) -mindepth 1 -name Makefile -type f -printf "%h\n")
 endif
